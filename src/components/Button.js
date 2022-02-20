bcdc87a2b5c2980a042799d1919a19f6a30afb08
kaperrdenden <weather2sogood@gmail.com>
2022-02-21 00:50:37 +0900

    classNames() 사용해서 class조건부로 지정

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Button.scss';
-
-function Button({children}){
-    return (<button className='Button'>{children}</button>)
+import classNames from 'classnames';
+function Button({children, size}){
+    return (<button className={classNames('Button', size)}>{children}</button>)
 }
+
+Button.defaultProps = {
+    size: 'medium',
+};
 
 export default Button;
 
