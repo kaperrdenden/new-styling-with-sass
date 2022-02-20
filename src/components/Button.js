7d5e8f293c56ddc8405fb7c71eb5e983bc06e2b6
kaperrdenden <weather2sogood@gmail.com>
2022-02-21 00:57:13 +0900

    color에 따른 css 작성하기

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Button.scss';
 import classNames from 'classnames';
-function Button({children, size}){
-    return (<button className={classNames('Button', size)}>{children}</button>)
+function Button({children, size, color}){
+    return (<button className={classNames('Button', size, color)}>{children}</button>)
 }
 
 Button.defaultProps = {
     size: 'medium',
+    color: 'blue',
 };
 
 export default Button;
