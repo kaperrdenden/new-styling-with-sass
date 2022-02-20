dc98fbb5a2908c67420bdd3b881d118d610229c9
kaperrdenden <weather2sogood@gmail.com>
2022-02-21 01:37:24 +0900

    fullWidth class를 추가

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
         <Button color="gray" outline>BUTTON</Button>
         <Button size='small' color="pink" outline>BUTTON</Button>
       </div>
+      <div className='buttons'>
+        <Button size='large' color="blue" fullWidth>BUTTON</Button>
+        <Button color="gray" fullWidth>BUTTON</Button>
+        <Button size='small' color="pink" fullWidth>BUTTON</Button>
+      </div>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.scss';
 import classNames from 'classnames';
-function Button({ children, size, color, outline }) {
+function Button({ children, size, color, outline, fullWidth }) {
     return (
-      <button className={classNames('Button', size, color, { outline })}>
+      <button className={classNames('Button', size, color, { outline, fullWidth })}>
                                     {/* outline의 값이 true일때만 
                                         button에 outline class가 적용된다
                                         조건식같은 느낌?
